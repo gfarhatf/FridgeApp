5afdd34165da1136df3bbb1167a698f499fbdf4e
wongivyw <iwong2598@gmail.com>
2021-11-10 14:41:03 -0800

    mainActivity: fixed editTexts and added toast messages to login and register buttons

--- app/src/main/java/com/example/fridgeapp/MainActivity.java
+++ app/src/main/java/com/example/fridgeapp/MainActivity.java
@@ -4,24 +4,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 //  LoginActivity
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    //User input EditTexts
+    private EditText usernameEditText, passwordEditText;
     //Text views
+    private TextView registerTextView;
 
     //Buttons
+    private Button loginButton;
 
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
 
+        usernameEditText = (EditText) findViewById(R.id.editTextUsername);
+        passwordEditText = (EditText) findViewById(R.id.editTextPassword);
+
+        registerTextView = (TextView) findViewById(R.id.registerTextView);
+        registerTextView.setOnClickListener(this);
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-
+        if (view == loginButton) {
+            Toast.makeText(this, "login clicked", Toast.LENGTH_SHORT).show();
+        }
+        if (view == registerTextView) {
+            Toast.makeText(this, "register clicked", Toast.LENGTH_SHORT).show();
+        }
     }
 }
