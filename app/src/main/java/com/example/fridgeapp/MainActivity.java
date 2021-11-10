a8f975ab2a31804257347cc820eed827ca5d0ec7
gfarhatf <94024436+gfarhatf@users.noreply.github.com>
2021-11-09 18:25:02 -0800

    deleted the universe text view and deleted the extra comments in the main activity

--- app/src/main/java/com/example/fridgeapp/MainActivity.java
+++ app/src/main/java/com/example/fridgeapp/MainActivity.java
@@ -7,19 +7,21 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    //Text views
+
+    //Buttons
+
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //change in onCreate by Ivy
 
 
     }
 
     @Override
     public void onClick(View view) {
-        //IVY's comment3
 
-        //this is a test to see if we have any problems in the main activity
     }
 }
