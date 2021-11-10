b9dc18f93104af981c56ed2285d6bc741f70c0c5
gfarhatf <94024436+gfarhatf@users.noreply.github.com>
2021-11-09 17:39:45 -0800

    added a comment in onClick method

--- app/src/main/java/com/example/fridgeapp/MainActivity.java
+++ app/src/main/java/com/example/fridgeapp/MainActivity.java
@@ -17,6 +17,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        
+        //do something with the button
     }
 }
