0620086241aac22df642c759207683c6eb772d2c
gfarhatf <94024436+gfarhatf@users.noreply.github.com>
2021-11-09 17:38:07 -0800

    added onclick listener method

--- app/src/main/java/com/example/fridgeapp/MainActivity.java
+++ app/src/main/java/com/example/fridgeapp/MainActivity.java
@@ -3,13 +3,20 @@ package com.example.fridgeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
         
     }
 }
