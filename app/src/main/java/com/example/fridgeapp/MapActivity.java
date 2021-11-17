4bdb62d917bc64dcda93c4e635c51b2a7e382681
wongivyw <iwong2598@gmail.com>
2021-11-16 17:32:41 -0800

    Added map xml and java files and changed map id from home to map

---
+++ app/src/main/java/com/example/fridgeapp/MapActivity.java
@@ -0,0 +1,17 @@
+package com.example.fridgeapp;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.AdapterView;
+
+public class MapActivity extends Activity implements AdapterView.OnItemClickListener, View.OnClickListener {
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+}
