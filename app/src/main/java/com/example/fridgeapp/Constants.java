db7f53a215b4cec1a83c997af0f3dcafaf851d9b
gfarhatf <94024436+gfarhatf@users.noreply.github.com>
2021-11-22 01:58:12 -0800

    added the constants for the ingredients table

--- app/src/main/java/com/example/fridgeapp/Constants.java
+++ app/src/main/java/com/example/fridgeapp/Constants.java
@@ -2,10 +2,19 @@ package com.example.fridgeapp;
 
 public class Constants {
     public static final String DATABASE_NAME = "fridgedatabase";
+
     public static final String TABLE_NAME = "USERSTABLE";
     public static final String UID = "_id";
     public static final String USERNAME = "Username";
     public static final String PASSWORD = "Password";
     public static final String EMAIL = "Email";
     public static final int DATABASE_VERSION = 1;
+
+    public static final String INGREDIENT_TABLE_NAME = "INGREDIENT_TABLE";
+    public static final String INGREDIENT_UID = "_id";
+    public static final String INGREDIENT_NAME = "Name";
+    public static final String INGREDIENT_TYPE = "Type";
+    public static final String INGREDIENT_QUANTITY = "Quantity";
+    public static final String INGREDIENT_IMAGE = "Image";
+
 }
