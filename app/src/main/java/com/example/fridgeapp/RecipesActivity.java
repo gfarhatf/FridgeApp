09278ccc9bbde6ace90774d7032dde6a0d7a1c95
gfarhatf <94024436+gfarhatf@users.noreply.github.com>
2021-11-22 19:34:48 -0800

    added the recipes website implicit intent

--- app/src/main/java/com/example/fridgeapp/RecipesActivity.java
+++ app/src/main/java/com/example/fridgeapp/RecipesActivity.java
@@ -2,6 +2,7 @@ package com.example.fridgeapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,7 +22,14 @@ public class RecipesActivity extends Activity implements View.OnClickListener{
 
     @Override
     public void onClick(View view) {
-
+//        code reference:
+//        https://stackoverflow.com/questions/5026349/how-to-open-a-website-when-a-button-is-clicked-in-android-application
+        
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        intent.setData(Uri.parse("https://www.allrecipes.com/recipes/"));
+        startActivity(intent);
     }
 
     public void goToFridgeActivity (View view) {
