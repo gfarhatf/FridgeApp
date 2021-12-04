f48dc6926a15be763c61d94a884a02c1c8365a83
wongivyw <iwong2598@gmail.com>
2021-12-03 23:11:58 -0800

    ambient temperature detection added to profile activity // BUT my phone doesn't have that sensor ...

--- app/src/main/java/com/example/fridgeapp/ProfileActivity.java
+++ app/src/main/java/com/example/fridgeapp/ProfileActivity.java
@@ -6,16 +6,30 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class ProfileActivity extends Activity implements View.OnClickListener {
+import java.util.List;
+
+public class ProfileActivity extends Activity implements View.OnClickListener, SensorEventListener {
+
+    //record temperature with ambient temperature sensor
+    SensorManager mySensorManager;
+    Sensor myTemperatureSensor;
 
     TextView username, password, email;
     Button logout;
+
+    float newTemp;
+
+    TextView previousTemp, currentTemp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +37,19 @@ public class ProfileActivity extends Activity implements View.OnClickListener {
         setContentView(R.layout.activity_profile);
 //        Toast.makeText(this, "onCreate-profile", Toast.LENGTH_SHORT).show();
 
+        previousTemp = (TextView) findViewById(R.id.previousTempVal);
+        currentTemp = (TextView) findViewById(R.id.currentTempVal);
+
         username = (TextView) findViewById(R.id.username);
         password = (TextView) findViewById(R.id.password);
         email = (TextView) findViewById(R.id.email);
 
         logout = (Button) findViewById(R.id.logoutButton);
+
+        //get reference to sensor an attach a listener - (acquire sensors late - release early)
+        mySensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        //get reference to the accelerometer sensor
+        myTemperatureSensor = mySensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
 
         //display username, password in *s and email that is stored in shared preferences
         SharedPreferences sharedPrefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
@@ -52,8 +74,36 @@ public class ProfileActivity extends Activity implements View.OnClickListener {
 
     }
 
+    @Override protected void onResume() {
+//        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+        super.onResume();
+        //listener for the ambient temperature sensor
+        mySensorManager.registerListener(this, myTemperatureSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        //unregister listener - release the sensor
+        mySensorManager.unregisterListener(this);
+        super.onPause();
+    }
+
     @Override
     public void onClick (View view) {
+    }
+
+    public void recordTemperatureButton (View view) {
+        //record temperature
+//        Toast.makeText(this, "record temperature", Toast.LENGTH_SHORT).show();
+        if (myTemperatureSensor == null) {
+            Toast.makeText(this, "Sensor not available", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            String oldTemp = currentTemp.getText().toString();
+//          String newTemp = "no data";
+            previousTemp.setText(oldTemp);
+            currentTemp.setText(Float.toString(newTemp));
+        }
     }
 
     public void logoutButton (View view) {
@@ -80,5 +130,19 @@ public class ProfileActivity extends Activity implements View.OnClickListener {
     public void goToMapActivity (View view) {
         Intent i = new Intent(this, MapActivity.class);
         startActivity(i);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        int type = sensorEvent.sensor.getType();
+        if (type == Sensor.TYPE_AMBIENT_TEMPERATURE) {
+            float[] temp_val = sensorEvent.values;
+            newTemp = temp_val[0];
+            Toast.makeText(this, "Temp = " + temp_val[0], Toast.LENGTH_SHORT).show();
+        }    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
